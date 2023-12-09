7bc63840bfaf76c00cdb1af5db71c5b817081124
maykon-jds <maykondias2001@gmail.com>
2023-12-09 12:56:29 -0300

    organize the repository by the base book session

---
+++ 14-exercicios/1-sem-phpunit/calculadora/Test/TestCalculadora.class.php
@@ -0,0 +1,32 @@
+<?php 
+
+require_once __DIR__ . '/../Class/Calculadora.class.php';
+
+
+class TestCalculadora {
+
+    public function testSomar(int $valor1, int $valor2, int $valorEsperado) {
+
+        $valorEsperado = $valorEsperado;
+
+        $calculadora = new Calculadora();
+
+        $valorRetornado = $calculadora->somar($valor1, $valor2);
+
+        return $valorEsperado === $valorRetornado;
+    }
+
+}
+
+$testCalculadora = new TestCalculadora();
+
+var_dump($testCalculadora->testSomar(5,5,10));
+var_dump($testCalculadora->testSomar(5,2,7));
+var_dump($testCalculadora->testSomar(2,2,4));
+var_dump($testCalculadora->testSomar(3,5,8));
+var_dump($testCalculadora->testSomar(0,0,0));
+var_dump($testCalculadora->testSomar(0,1,1));
+var_dump($testCalculadora->testSomar(-1,1,0));
+
+
+?>
